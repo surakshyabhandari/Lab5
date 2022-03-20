9ac0e7bdd32ebc4b788cbce7a222f06497f00541
surakshyabhandari <51433451+surakshyabhandari@users.noreply.github.com>
2022-03-20 13:32:48 +0545

    coding challenge

---
+++ app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.shoppinglist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int TEXT_REQUEST = 1;
+    private ConstraintLayout parent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        parent = (ConstraintLayout)  findViewById(R.id.layout);
+
+        if (savedInstanceState != null){
+            for (int index=0; index<parent.getChildCount(); index++){
+                final TextView child = (TextView) parent.getChildAt(index);
+                boolean isVisible = savedInstanceState.getBoolean(Integer.toString(index) + "_visible");
+
+                if (isVisible) {
+                    child.setVisibility(View.VISIBLE);
+                    String text = savedInstanceState.getString(Integer.toString(index) + "_text");
+                    child.setText(text);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState (Bundle outState){
+        super.onSaveInstanceState(outState);
+        for (int index=0; index<parent.getChildCount(); index++){
+            final TextView child = (TextView) parent.getChildAt(index);
+
+            if (child.getVisibility() == View.VISIBLE) {
+                outState.putBoolean(Integer.toString(index) + "_visible", true);
+                outState.putString(Integer.toString(index) + "_text", child.getText().toString());
+            }
+
+        }
+    }
+
+
+    public void addItem(View view) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int request, int result, Intent data){
+        super.onActivityResult(request, result, data);
+        if(request == TEXT_REQUEST){
+            if(result == RESULT_OK){
+                String item = data.getStringExtra(MainActivity2.EXTRA_REPLY);
+
+                for(int index = 0; index < parent.getChildCount(); index++){
+                    final TextView child = (TextView) parent.getChildAt(index);
+
+                    if (child.getVisibility() == View.INVISIBLE) {
+                        child.setText(Integer.toString(index+1) + ". " + item);
+                        child.setVisibility(View.VISIBLE);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/shoppinglist/MainActivity2.java
@@ -0,0 +1,30 @@
+package com.example.shoppinglist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    public static final String EXTRA_REPLY =
+            "com.example.android.ShoppingList.extra.REPLY";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void addItem(View view) {
+        Button itembtn = (Button) (view);
+        String item = itembtn.getText().toString();
+
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_REPLY, item);
+        setResult(RESULT_OK,intent);
+        finish();
+    }
+}
